b6b1afa5fc72dbfcd6d41b6fe374105fc2a7067d
Etherealxx <64251396+etherealxx@users.noreply.github.com>
2024-11-15 11:12:58 +0700

    comment and prettify

--- scripts/DropdownGroup.gd
+++ scripts/DropdownGroup.gd
@@ -9,25 +9,29 @@ func _ready():
 	%JobDesc.set_text("")
 	menupopup.id_pressed.connect(_on_popup_item_pressed)
 
+func initialize_menupopup():
+		for job_item in job_table.keys():
+			# add the jobname to the menu popup
+			var job_name = job_table[job_item]["JOBNAME"] 
+			menupopup.add_item(job_name)
+			
+			# keep the jobname-id pair for reference later
+			job_key_id_val[job_name] = int(job_table[job_item]["ID"])
+
+
 func _on_popup_item_pressed(id):
 	var index = menupopup.get_item_index(id)
 	var job_name = menupopup.get_item_text(index)
 	%MenuButton.text = job_name
+	
+	# fill the labels
 	var job_id : int = job_key_id_val[job_name]
 	%JobName.text = job_table[job_id]["JOBNAME"]
 	%JobDesc.text = job_table[job_id]["DESCRIPTION"]
 	
+	# add skills to the item list
 	var all_skills : String = job_table[job_id]["SKILLS"]
 	var skill_list : PackedStringArray = all_skills.split(";")
 	$ItemList.clear()
 	for skill in skill_list:
 		$ItemList.add_item(skill.strip_edges())
-
-#func add_item_to_menupopup(item : String):
-	#menupopup.add_item(item)
-
-func initialize_menupopup():
-		for job_item in job_table.keys():
-			var job_name = job_table[job_item]["JOBNAME"]
-			menupopup.add_item(job_name)
-			job_key_id_val[job_name] = int(job_table[job_item]["ID"])
--- scripts/job_dropdown.gd
+++ scripts/job_dropdown.gd
@@ -2,7 +2,8 @@ extends Control
 
 @export_file("*.csv") var joblist_file = ""
 
-var job_table := Dictionary()
+var job_table := Dictionary() # job id as key, and the whole table row as value
+
 var job_properties := Array()
 
 func _ready():
@@ -14,44 +15,31 @@ func _ready():
 		# for non-translation csv data
 		var next_csv_line := file.get_csv_line(",")
 		var firstrow_init := false
-		while next_csv_line != PackedStringArray([""]):
-			#print(str(next_csv_line))
-			
-			#var column = 1 
+		while next_csv_line != PackedStringArray([""]): # iterate every row
 			var i = 0
-			var job_detail := Dictionary()
+			
+			# contains a single row with the column name as key
+			var job_detail := Dictionary() 
+			
+			# iterate on each item in row
+			# and fill the job_detail
 			for item in next_csv_line:
-				if !firstrow_init:
-					#job_table[item] = Array()
-					#job_table[column] = item
-					#column += 1
+				if !firstrow_init: 
+					# keep the value of the first row as reference
 					job_properties.append(item)
 				else:
-					job_detail[job_properties[i]] = item
+					# job_properties[i] is current column's name
+					job_detail[job_properties[i]] = item 
 					i += 1
 			
 			if firstrow_init:
-				print(str(job_detail))
-
-				var job_index = int(job_detail[job_properties[0]]) # leftmost item
+				# leftmost item, in this case, ID
+				var job_index = int(job_detail[job_properties[0]]) 
 			
 				job_table[job_index] = job_detail
-					
-			#var column = 1 
-			#for item in next_csv_line:
-				#if !firstrow_init:
-					#job_table[item] = Array()
-					#job_table[column] = item
-					#column += 1
-				#else:
-					#job_table[job_table[column]][]
 					
 			firstrow_init = true
 			next_csv_line = file.get_csv_line(",")
 		
-		#print(str(job_table))
-		#print(job_table[1])
-		%DropdownGroup.job_table = job_table
+		%DropdownGroup.job_table = job_table # sync the job_table with the dropdown group
 		%DropdownGroup.initialize_menupopup()
-			
-		
